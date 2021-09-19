e2f32fb9c60db29d6532faa3335d2a879e39b397
Denis Shingala <77537258+Denis-Shingala@users.noreply.github.com>
2021-09-19 13:16:30 +0530

    Update connect.php

--- PHP-Files/connect.php
+++ PHP-Files/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-    $host='localhost';
-    $user='root';
-    $project='online_quiz';
-    $ps='';
+    $host='sql5.freesqldatabase.com';
+    $user='sql5438305';
+    $project='sql5438305';
+    $ps='Fn66l1sWfz';
 
 ?>
